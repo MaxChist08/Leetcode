55d749ba5ac55e6e81a175b7f74af92eefaf715f
MaxChist08 <mchistiakov060908@mail.ru>
2025-03-18 12:50:51 +0300

    Add files via upload

--- 0059. Spiral Matrix II.py
+++ 0059. Spiral Matrix II.py
@@ -1,23 +1,21 @@
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
+def generate_matrix(n):
+    A = []
+    for i in range(0, n):
+        A.append([0] * n)
 
-        A = []
-        for i in range(0, n):
-            A.append([0] * n)
+    num = 1
+    for i in range(n // 2 + n % 2):
+        for j in range(i, n - i):
+            A[i][j] = num
+            num += 1
+        for j in range(i + 1, n - i):
+            A[j][n - i - 1] = num
+            num += 1
+        for j in range(n - i - 2, i - 1, -1):
+            A[n - i - 1][j] = num
+            num += 1
+        for j in range(n - i - 2, i, -1):
+            A[j][i] = num
+            num += 1
 
-        num = 1
-        for i in range(n // 2 + n % 2):
-            for j in range(i, n - i):
-                A[i][j] = num
-                num += 1
-            for j in range(i + 1, n - i):
-                A[j][n - i - 1] = num
-                num += 1
-            for j in range(n - i - 2, i - 1, -1):
-                A[n - i - 1][j] = num
-                num += 1
-            for j in range(n - i - 2, i, -1):
-                A[j][i] = num
-                num += 1
-
-        return A
+    return A
