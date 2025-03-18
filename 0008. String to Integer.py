be85f16d0175cf0682f2dd169818a4c6b888d754
MaxChist08 <mchistiakov060908@mail.ru>
2025-03-18 08:35:33 +0300

    Add files via upload

--- 0008. String to Integer.py
+++ 0008. String to Integer.py
@@ -1,34 +1,32 @@
-class Solution(object):
-    def myAtoi(self, s):
-        numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
+def my_atoi(s):
+    numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 
-        i = 0
-        ans = "0"
-        minus = False
+    i = 0
+    ans = "0"
+    minus = False
 
-        while i < len(s) and s[i] == " ":
-            i += 1
+    while i < len(s) and s[i] == " ":
+        i += 1
 
-        if i < len(s) and s[i] == '-':
-            i += 1
-            minus = True
-        elif i < len(s) and s[i] == '+':
-            i += 1
-        elif i < len(s) and s[i] in numbers:
-            ans += s[i]
-            i += 1
+    if i < len(s) and s[i] == '-':
+        i += 1
+        minus = True
+    elif i < len(s) and s[i] == '+':
+        i += 1
+    elif i < len(s) and s[i] in numbers:
+        ans += s[i]
+        i += 1
 
-        for j in range(i, len(s)):
-            if s[j] not in numbers:
-                break
-            ans += str(s[j])
+    for j in range(i, len(s)):
+        if s[j] not in numbers:
+            break
+        ans += str(s[j])
 
-        if minus:
-            if (-1 * (int(ans))) < (-2 ** 31):
-                return -2 ** 31
-            return -1 * (int(ans))
-        else:
-            if int(ans) > (2 ** 31 - 1):
-                return 2 ** 31 - 1
-            return int(ans)
-
+    if minus:
+        if (-1 * (int(ans))) < (-2 ** 31):
+            return -2 ** 31
+        return -1 * (int(ans))
+    else:
+        if int(ans) > (2 ** 31 - 1):
+            return 2 ** 31 - 1
+        return int(ans)
