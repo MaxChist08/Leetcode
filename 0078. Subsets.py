d29938e7d0ca5c750e4687730acb99d49a701c30
MaxChist08 <mchistiakov060908@mail.ru>
2025-03-18 12:53:02 +0300

    Add files via upload

--- 0078. Subsets.py
+++ 0078. Subsets.py
@@ -1,22 +1,20 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        def f(nums, a, lst):
-            lst.append(list())
-            for x in a:
-                lst[-1].append(x)
+def subsets(nums):
+    def f(nums, a, lst):
+        lst.append(list())
+        for x in a:
+            lst[-1].append(x)
 
-            if len(nums) == 0:
-                return
+        if len(nums) == 0:
+            return
 
-            for i in range(len(nums)):
-                a.append(nums[i])
-                f(nums[i+1:], a, lst)
-                a.pop()
+        for i in range(len(nums)):
+            a.append(nums[i])
+            f(nums[i + 1:], a, lst)
+            a.pop()
 
-        lst = list()
-        a = list()
+    lst = list()
+    a = list()
 
-        f(nums, a, lst)
+    f(nums, a, lst)
 
-        return lst
-
+    return lst
