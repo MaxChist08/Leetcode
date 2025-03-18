e6a0c4d9c26d4d29a510e2e6a492529cfdb1a057
MaxChist08 <mchistiakov060908@mail.ru>
2025-03-18 08:33:23 +0300

    Add files via upload

--- 0007. Reverse Integer.py
+++ 0007. Reverse Integer.py
@@ -1,35 +1,14 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        """a = str(x)
-        k = True
+def reverse(x):
+    a = str(x)
+    k = ""
+    if a[0] == '-':
+        k = '-'
+        a = a[1::]
 
-        if a[0] == '-':
-            a = a[:0:-1]
-            k = False
-        else:
-            a = a[::-1]
+    a = a[::-1]
+    x = int(k + a)
 
-        if k:
-            x = int(a)
-        else:
-            x = int(a) * -1
-
-        if x < pow(-2, 31) or x > pow(2, 31) - 1:
-            return 0
-        else:
-            return x"""
-
-        a = str(x)
-        k = ""
-
-        if a[0] == '-':
-            k = '-'
-            a = a[1::]
-
-        a = a[::-1]
-        x = int(k + a)
-
-        if x < pow(-2, 31) or x > pow(2, 31) - 1:
-            return 0
-        else:
-            return x
+    if x < pow(-2, 31) or x > pow(2, 31) - 1:
+        return 0
+    else:
+        return x
