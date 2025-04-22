0de138c2567f268bb9bfd0e535e62ced14957b51
MaxChist08 <mchistiakov060908@mail.ru>
2025-04-23 00:37:43 +0300

    Add files via upload

--- 0047. Permutations II.py
+++ 0047. Permutations II.py
@@ -1,23 +1,20 @@
-def multiply(num1, num2):
-    nums = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "0": 0}
-    string = {1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 0: "0"}
+def permute_unique(nums):
+    def all_permutations(lst, nums, nums1):
+        if len(lst) == len_nums:
+            ans.append(list())
+            for x in lst:
+                ans[-1].append(x)
+            return
+        for i in range(len(nums)):
+            if i == 0 or nums[i - 1] != nums[i]:
+                lst.append(nums[i])
+                all_permutations(lst, nums[:i] + nums[i + 1:], len_nums)
+                lst.pop()
 
-    num1int = 0
-    num2int = 0
+    ans = list()
 
-    for x in num1:
-        num1int = num1int * 10 + nums[x]
+    len_nums = len(nums)
+    nums = sorted(nums)
 
-    for x in num2:
-        num2int = num2int * 10 + nums[x]
-
-    ans = num1int * num2int
-    ans_str = ""
-
-    while ans > 9:
-        ans_str = string[ans % 10] + ans_str
-        ans //= 10
-
-    ans_str = string[ans] + ans_str
-
-    return ans_str
+    all_permutations(list(), nums, len_nums)
+    return ans
