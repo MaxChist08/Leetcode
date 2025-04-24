216259784793964f9c18828392286338d3327e2e
MaxChist08 <mchistiakov060908@mail.ru>
2025-04-24 09:15:59 +0300

    Add files via upload

---
+++ 0875. Koko Eating Bananas.py
@@ -0,0 +1,22 @@
+def min_eating_speed(piles, h):
+    def f(speed):
+        time = 0
+        for x in piles:
+            time += x // speed
+            if x % speed != 0:
+                time += 1
+        return time
+
+    start = 1
+    finish = 0
+    for x in piles:
+        finish += x
+
+    while start <= finish:
+        middle = (start + finish) // 2
+        print(f(middle), start, finish)
+        if f(middle) <= h:
+            finish = middle - 1
+        else:
+            start = middle + 1
+    return start
