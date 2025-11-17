9938bd3135da3499b8efa1cb24a3ed85a2636e39
MaxChist08 <mchistiakov060908@mail.ru>
2025-11-17 11:23:13 +0300

    Add files via upload

---
+++ 0400. Nth Digit.py
@@ -0,0 +1,11 @@
+def find_Nth_Digit(n):
+    k = 0
+
+    while n > 9 * 10 ** k * (k + 1):
+        n -= 9 * 10 ** k * (k + 1)
+        k += 1
+
+    n -= 1
+    num = 10 ** k + n // (k + 1)
+
+    return int(str(num)[n % (k + 1)])
